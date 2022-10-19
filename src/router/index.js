a8646685321fbcab0d1bde5fb4132715315cbe6b
BoboYan <byan@unomaha.edu>
2022-10-19 11:52:08 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     {
-            path: '/list',
+            path: '/',
             name: 'List',
             component: () => import('../components/List')
         },
